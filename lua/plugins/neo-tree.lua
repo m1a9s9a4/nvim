ddd00dd882efdee70873636fb917abf0d0da0943
masafumi kato <m1a9s9a4.dev@gmail.com>
2026-01-11 01:19:35 +0900

    refact: add neo-tree

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,22 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+  },
+  cmd = "Neotree",
+  keys = {
+    { "<leader>e", "<cmd>Neotree toggle<cr>", desc = "NeoTree" },
+  },
+  opts = {
+    filesystem = {
+      follow_current_file = {
+        enabled = true,
+      },
+      use_libuv_file_watcher = true,
+    },
+  },
+}
